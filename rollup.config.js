85d4b106524caf2f85d2c4dfbea99545249764a0
Adenekan Wonderful <adenekanwonderful41@gmail.com>
2022-02-05 05:07:02 +0100

    🚀 (chore): added typescript to rollup flow

--- rollup.config.js
+++ rollup.config.js
@@ -1,67 +1,108 @@
-import babel from 'rollup-plugin-babel';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import multiInput from 'rollup-plugin-multi-input';
+/* -------------------------------------------------------------------------- */
+/*                            External Dependencies                           */
+/* -------------------------------------------------------------------------- */
+
+import pluginTypescript from '@rollup/plugin-typescript';
+import pluginCommonjs from '@rollup/plugin-commonjs';
+import pluginNodeResolve from '@rollup/plugin-node-resolve';
+
 import { terser } from 'rollup-plugin-terser';
+import multiInput from 'rollup-plugin-multi-input';
 
-export default {
-  external: ['react'],
-  input: ['src/**/*.js'],
+/* -------------------------- Internal Dependencies ------------------------- */
+import pkg from './package.json';
+import defaultTsConfig from './tsconfig.json';
 
-  output: [
-    {
-      dir: 'bundle-es',
-      format: 'es',
-      name: 'bundle',
-      plugins: [
-        terser({
-          include: ['src/**/*.js'],
-          exclude: ['index.js'],
-        }),
-      ],
-      globals: {
-        react: 'React',
-      },
-    },
+const moduleName = pkg.name.replace(/^@.*\//, '');
+const inputFileName = ['src/**/*.ts'];
 
-    {
-      dir: 'bundle-cjs',
-      format: 'cjs',
-      exports: 'named',
-      name: 'bundle',
-      plugins: [
-        terser({
-          include: ['src/**/*.js'],
-          exclude: ['index.js'],
-        }),
-      ],
-    },
-    {
-      name: 'bundle',
-      dir: 'bundle-umd',
-      format: 'esm',
-      exports: 'named',
-      plugins: [
-        terser({
-          include: ['src/**/*.js'],
-          exclude: ['index.js'],
-        }),
-      ],
-      globals: {
-        react: 'React',
-      },
-    },
-  ],
+const author = pkg.author;
+
+const bundles = {
+  es: 'dist/bundle-es',
+  cjs: 'dist/bundle-cjs',
+  browser: 'dist/bundle-browser',
+};
+
+const banner = `
+  /**
+   * ${moduleName}.js 
+   * @summary ${pkg.description}
+   * @version v${pkg.version}
+   * @author  ${author}
+   * @license Released under the ${pkg.license} license.
+   * @copyright Adenekan Wonderful 2021
+   */
+`;
+
+const pluginsSetups = bundle => ({
+  external: ['react', 'react-dom'],
   plugins: [
     multiInput(),
-    babel({
-      exclude: 'node_modules/**',
+    pluginTypescript({
+      ...defaultTsConfig.compilerOptions,
+      ...{
+        declaration: true,
+        emitDeclarationOnly: true,
+        outDir: `${bundle}`,
+        declarationDir: `${bundle}`,
+        exclude: ['node_modules', 'dist'],
+      },
     }),
-    resolve({
-      jsnext: true,
-      main: true,
-      browser: true,
+
+    pluginCommonjs({
+      extensions: ['.ts', '.tsx'],
     }),
-    commonjs(),
+
+    pluginNodeResolve({
+      browser: bundle === bundles.browser ? true : false,
+    }),
   ],
-};
+});
+
+export default [
+  {
+    input: inputFileName,
+    output: [
+      {
+        name: moduleName,
+        dir: bundles.browser,
+        format: 'esm',
+        sourcemap: 'inline',
+        banner,
+        plugins: [terser()],
+      },
+    ],
+    ...pluginsSetups(bundles.browser),
+  },
+
+  // ES
+  {
+    input: inputFileName,
+    output: [
+      {
+        dir: bundles.es,
+        format: 'es',
+        sourcemap: 'inline',
+        banner,
+        exports: 'named',
+      },
+    ],
+    ...pluginsSetups(bundles.es),
+  },
+
+  // CommonJS
+  {
+    input: inputFileName,
+    output: [
+      {
+        dir: bundles.cjs,
+        format: 'cjs',
+        sourcemap: 'inline',
+        banner,
+        exports: 'named',
+      },
+    ],
+    ...pluginsSetups(bundles.cjs),
+  },
+];
