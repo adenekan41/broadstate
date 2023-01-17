85b683b5e7543ee827678057013b127fe026cfe5
Adenekan Wonderful <adenekanwonderful41@gmail.com>
2023-01-17 22:34:51 +0100

    (chore): added `Observable` class to useBroad

--- src/use-broad.ts
+++ src/use-broad.ts
@@ -1,13 +1,13 @@
 /* -------------------------------------------------------------------------- */
 /*                             External Dependency                            */
 /* -------------------------------------------------------------------------- */
-import { IObservable } from 'create-observable';
 import { useState, useCallback } from 'react';
+import { Observable } from 'create-observable';
 
 /* -------------------------- Internal Dependencies ------------------------- */
 import useMount from './use-mount';
 
-const useBroadState = <T extends unknown>(store: IObservable<T>) => {
+const useBroadState = <T extends unknown>(store: Observable<T>) => {
   /**
    * Create a state to subscribe to and get the initial value
    * form the subscribed state
@@ -15,15 +15,14 @@ const useBroadState = <T extends unknown>(store: IObservable<T>) => {
   const [state, setGlobal] = useState<T>(store.get());
 
   /**
-   * Get the value of the subscribed state once component
-   * is mounted only
+   * Get the value of the subscribed state once component it's mounted only
    */
   useMount(() => {
     return store.subscribe(setGlobal);
   });
 
   /**
-   * Create an updater function that allows us to subcribe to
+   * Create an updater function that allows us to subscribe to
    * the state and update the observed value.
    */
   const setState = useCallback((state: T) => store.set(state), [store]);
@@ -31,7 +30,7 @@ const useBroadState = <T extends unknown>(store: IObservable<T>) => {
   /**
    * Return the value and updater
    */
-  return [state, setState] as const;
+  return [state, setState] as [T, typeof setState];
 };
 
 export default useBroadState;
