d043d9d37cea228b090a4312914539547a8d78d9
Adenekan Wonderful <adenekanwonderful41@gmail.com>
2023-01-17 22:34:10 +0100

    (chore): updated `useMount` method

--- src/use-mount.ts
+++ src/use-mount.ts
@@ -7,6 +7,11 @@ import { useEffect } from 'react';
  * Gives you the ability to run a function only when users
  * loads into the platform similar to a componentDidMount
  */
-const useMount = (effect: () => void) => useEffect(effect, []);
+const useMount = (effect: () => void) => {
+  useEffect(() => {
+    effect();
+    return () => {};
+  }, []);
+};
 
 export default useMount;
