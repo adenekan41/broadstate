79b9126ef8772545cfea56a9c4c143ba08731c14
Adenekan Wonderful <adenekanwonderful41@gmail.com>
2023-01-17 22:45:28 +0100

    (feat): updated `createObservable` function

--- src/create-observable.ts
+++ src/create-observable.ts
@@ -3,60 +3,68 @@
  * to observe and update the value
  */
 
-export interface IObservable<T> {
-  get: () => T;
-  set: (data: T) => void;
-  subscribe: (callback: (data: T) => void) => void;
+interface IObservable<T> {
+  get(): T;
+  set(newValue: T): void;
+  subscribe(listenerFunc: Listener<T>): void;
 }
 
-const createObservable = <T extends unknown>(
-  initialData: T
-): IObservable<T> => {
-  let listeners: Array<T> = [];
-  let value = initialData;
+interface Subscription {
+  unsubscribe(): void;
+}
 
-  // get Observed value
-  const get = (): T => value;
+type Listener<T> = (data: T) => void;
+
+export class Observable<T> implements IObservable<T> {
+  private readonly listeners: Set<Listener<T>> = new Set();
+  private value: Readonly<T>;
+
+  constructor(initialData: T) {
+    this.value = initialData;
+  }
+
+  /**
+   * Get the observed value from the observable object
+   *
+   * @return  {T} The observed value
+   */
+  get(): T {
+    return this.value;
+  }
 
   /**
    * Create a setter for the observed value so we can later
-   * on subcribe to that value
+   * subscribe to that value.
    *
+   * @param newValue The new value to be set
    */
-  const set = (newValue: T) => {
-    /**
-     * throw and early return if former value is equals to the new value
-     */
-    if (value === newValue) return;
-
-    value = newValue;
-    /**
-     * At this point we have a subcriber to the value
-     */
-    listeners.forEach((listener: any) => listener(value));
-  };
-
-  /**
-   * Unsubscribe from global listener list and remove the listener
-   */
-  const unsubscribe = (listenerFunc: (data: T) => void): void => {
-    listeners = listeners.filter(listener => listener !== listenerFunc);
-    return;
-  };
+  set(newValue: T): void {
+    if (this.value === newValue) return;
+    this.value = newValue;
+    this.listeners.forEach((listener: Listener<T>) => listener(this.value));
+  }
 
   /**
    * Subscribe to global listener list and add the listener
+   *
+   * @param   {Listener<T>}  listenerFunc
+   * @return  {<T><Subscription>}
    */
-  const subscribe = (listenerFunc: (callback: T) => void): void | unknown => {
-    listeners.push(listenerFunc as T);
-    return () => unsubscribe(listenerFunc);
-  };
+  subscribe(listenerFunc: Listener<T>): Subscription {
+    if (!this.listeners.has(listenerFunc)) this.listeners.add(listenerFunc);
+    return {
+      unsubscribe: () => {
+        this.listeners.delete(listenerFunc);
+      },
+    };
+  }
+}
 
-  return {
-    get,
-    set,
-    subscribe,
-  };
+/**
+ * For backward compatibility with the previous version of the library
+ */
+const createObservable = <T>(initialData: T): Observable<T> => {
+  return new Observable<T>(initialData);
 };
 
 export default createObservable;
